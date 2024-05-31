60360c6874366c9a36cf69223b84d9e610fd83da
“Fire-Dragons” <jiangzhaolong.hn@gmail.com>
2024-05-31 16:27:16 +0800

    更改静态路径，可以搭配nginx等进行路由转发

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,13 @@
 const path = require('path')
-
+const dev = process.env.NODE_ENV === 'development'
+ASSET_PATH = dev ? '/' : `/subweb`
+process.env.VUE_APP_ASSET_PATH = ASSET_PATH
 function resolve(dir) {
   return path.join(__dirname, dir)
 }
 
 module.exports = {
+    publicPath: ASSET_PATH,
   css: {
     loaderOptions: {
       less: {
@@ -17,12 +20,12 @@ module.exports = {
     // set svg-sprite-loader
     config.module
       .rule('svg')
-      .exclude.add(resolve('src/icons'))
+      .exclude.add(resolve('./src/icons'))
       .end()
     config.module
       .rule('icons')
       .test(/\.svg$/)
-      .include.add(resolve('src/icons'))
+      .include.add(resolve('./src/icons'))
       .end()
       .use('svg-sprite-loader')
       .loader('svg-sprite-loader')
